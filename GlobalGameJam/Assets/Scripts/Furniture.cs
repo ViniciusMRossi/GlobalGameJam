0b1a8a263e476f67783ce0eeb1d9c88bf4909a41
VB <vitor.unicamp@gmail.com>
2019-01-26 12:07:21 -0200

    changed graphics settings, added prefabs

--- GlobalGameJam/Assets/Scripts/Furniture.cs
+++ GlobalGameJam/Assets/Scripts/Furniture.cs
@@ -2,18 +2,33 @@
 
 public class Furniture : MonoBehaviour
 {
+    protected Rigidbody _rigidBody;
+    protected Collider _collider;
+    
     // Start is called before the first frame update
     private void Awake()
     {
         gameObject.layer = LayerMask.NameToLayer("Furniture");
-        if (GetComponent<Collider>() == null)
+        if ((_collider = GetComponent<Collider>()) == null)
         {
             Debug.LogError("Missing collider");
         }
+        else
+        {
+            if (_collider.isTrigger)
+            {
+                Debug.LogWarning("Should not be a trigger, changing this for this run");
+                _collider.isTrigger = false;
+            }
+        }
 
-        if (GetComponent<Rigidbody>() == null)
+        if ((_rigidBody = GetComponent<Rigidbody>()) == null)
         {
             Debug.LogError("Missing collider");
+        }
+        else
+        {
+            
         }
     }
 }
--- GlobalGameJam/Assets/Scripts/ThrowableFurniture.cs
+++ GlobalGameJam/Assets/Scripts/ThrowableFurniture.cs
@@ -1,3 +1,29 @@
-﻿public class ThrowableFurniture : Furniture
+﻿using UnityEngine;
+
+public class ThrowableFurniture : Furniture
 {
+    private Transform houseParent;
+    public float maxRotationStrength;
+    
+    private void Start()
+    {
+        houseParent = transform.parent;
+    }
+
+    public void Throw(Vector3 force)
+    {
+        transform.SetParent(houseParent, false);
+        _rigidBody.AddForce(force, ForceMode.Impulse);
+        _rigidBody.AddTorque(Random.Range(0, maxRotationStrength) * Vector3.forward);
+    }
+
+    public void Pickup(Transform parent)
+    {
+        transform.SetParent(parent, false);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        
+    }
 }
