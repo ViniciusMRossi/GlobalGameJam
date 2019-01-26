5caa0b611f5996686a67789c890382378bbb5265
VB <vitor.unicamp@gmail.com>
2019-01-26 11:12:21 -0200

    Added layers and basic script for furniture

---
+++ GlobalGameJam/Assets/Scripts/Furniture.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Furniture : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        gameObject.layer = LayerMask.NameToLayer("Furniture");
+        if (GetComponent<Collider>() == null)
+        {
+            Debug.LogError("Missing collider");
+        }
+
+        if (GetComponent<Rigidbody>() == null)
+        {
+            Debug.LogError("Missing collider");
+        }
+    }
+}
---
+++ GlobalGameJam/Assets/Scripts/ThrowableFurniture.cs
@@ -0,0 +1,3 @@
+﻿public class ThrowableFurniture : Furniture
+{
+}
