4647e49834778aef94dc3fb82033fb1c15663bd3
VB <vitor.unicamp@gmail.com>
2019-01-26 13:01:18 -0200

    modofications on scrips

--- GlobalGameJam/Assets/Scripts/Furniture.cs
+++ GlobalGameJam/Assets/Scripts/Furniture.cs
@@ -31,4 +31,36 @@ public class Furniture : MonoBehaviour
             
         }
     }
+    
+    private Transform houseParent;
+    public float maxRotationStrength;
+    
+    private void Start()
+    {
+        houseParent = transform.parent;
+    }
+
+    public void Throw(Vector3 force)
+    {
+        transform.SetParent(houseParent, false);
+        
+        _rigidBody.isKinematic = false;
+        _rigidBody.AddForce(force, ForceMode.Impulse);
+        _rigidBody.AddTorque(Random.Range(0, maxRotationStrength) * Vector3.forward);
+    }
+
+    public void Pickup(Transform parent)
+    {
+        transform.SetParent(parent, false);
+        _rigidBody.isKinematic = true;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Player player;
+        if ((player = other.gameObject.GetComponent<Player>()) != null)
+        {
+            player.OnHit(_rigidBody.mass * _rigidBody.velocity.magnitude);
+        }
+    }
 }
